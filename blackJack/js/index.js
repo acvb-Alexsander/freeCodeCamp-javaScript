6e59243799a4819ab200c774f7cb2f1b995aa4d8
acvb-Alexsander <acvb.developer@gmail.com>
2026-06-18 12:32:36 -0300

    criando botão de mais uma carta

--- blackJack/js/index.js
+++ blackJack/js/index.js
@@ -5,49 +5,48 @@ const secondCard = Math.floor(Math.random() * (max - min + 1)) + min;
 let results = firstCard + secondCard;
 document.getElementById("card1").innerText = firstCard;
 document.getElementById("card2").innerText = secondCard;
-
+let oneMoreCard = false;
 let hasBlackJack = false;
 let isAlive = true;
 let sum = document.getElementById("sum");
 let startGame = document.getElementById("game");
 let messageEl = document.getElementById("message-el");
+let newCard = document.getElementById("nova-carta");
 
 startGame.style.display = "none";
-
+newCard.style.display = "none";
 sum.innerText = results;
 
 function vitoria() {
   startGame.style.display = "flex";
+  newCard.style.display = "flex";
   let buttonStart = document.getElementById("button-btn");
 
   messageEl.innerText = "Want another Card?";
   blackJack();
 }
 
-if (results < 21) {
-  console.log("do you want to draw another card");
-} else if (results === 21) {
-  console.log("BLACKJACK");
-  hasBlackJack = true;
-} else {
-  console.log("you're out of the game");
-  isAlive = false;
-}
-
 function blackJack() {
-  if (results == 21) {
-    alert("BLACKJACK");
-    document.getElementById("vitoria").innerText =
-      "sum: PARABÉNS VOCÊ VENCEU A MESA DE BLACKJACK!!!! CONSEGUIU OS " +
-      results;
+  if (results < 21) {
+    messageEl.textContent = "do you want to draw another card?";
+  } else if (results === 21) {
+    messageEl.textContent = "BlackJack ganhador, ganhador, galinha pro jantar";
+    hasBlackJack = true;
   } else {
-    alert("Derrota");
-    document.getElementById("vitoria").innerText =
-      "sum: Você conseguiu " + results + " o resultado dependera da mesa";
+    messageEl.textContent = "you're out of the game";
+    isAlive = false;
   }
 }
 
-function anotherCard() {}
+function anotherCard() {
+  let oneMoreCard = true;
+  const thirdCard = Math.floor(Math.random() * (max - min + 1)) + min;
+  results += thirdCard;
+  sum.innerText = results;
+  console.log(results);
+
+  blackJack();
+}
 
 console.log(hasBlackJack);
 console.log(isAlive);
